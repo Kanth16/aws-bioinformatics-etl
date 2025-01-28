04cc48d8db48351c9e7701ffedfed32b489950fb
kanth16 <kanthi1998@gmail.com>
2025-01-28 13:56:24 -0500

    Testing

--- Data_Extraction/lambda_function.py
+++ Data_Extraction/lambda_function.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     print("Hello")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World testing 123!')
+        'body': json.dumps('Hello World Deployed!')
     }
